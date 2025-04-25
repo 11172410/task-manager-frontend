bf56b040068783d00e0e363bd53030e67877f663
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-25 12:47:16 +0400

    create reusable delete modal

---
+++ src/components/DeleteModal.jsx
@@ -0,0 +1,32 @@
+import { Modal, ModalBody, Button } from "flowbite-react";
+
+import React from 'react'
+
+function DeleteModal({show, handleClose}) {
+    const [openModal, setOpenModal] = useState(false);
+  return (
+    <>
+    <Button onClick={() => setOpenModal(true)}>Toggle modal</Button>
+        <Modal show={openModal} size="md" onClose={()=>setOpenModal(false)} popup>
+        <ModalHeader />
+        <ModalBody>
+          <div className="text-center">
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this product?
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button color="failure" onClick={() => setOpenModal(false)}>
+                {"Yes, I'm sure"}
+              </Button>
+              <Button color="gray" onClick={() => setOpenModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </ModalBody>
+      </Modal>
+    </>
+  )
+}
+
+export default DeleteModal
