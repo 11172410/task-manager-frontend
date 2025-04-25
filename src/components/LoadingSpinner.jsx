3bcc8fb0f48d73a96a4464cc3788c73b1a954177
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-25 12:37:53 +0400

    create reusable spinner component

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+import { Spinner } from "flowbite-react";
+
+import React from 'react'
+
+function LoadingSpinner() {
+  return (
+    <Spinner color="info" aria-label="Loading..." />
+  )
+}
+
+export default LoadingSpinner
