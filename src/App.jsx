de09cd55cf9599963937bac0cfe43a3a971c34e1
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-25 13:00:25 +0400

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import { Button } from "flowbite-react";
-import api from './api/axiosDefaults';
-import { useEffect } from 'react';
-import LoadingSpinner from './components/LoadingSpinner';
-import DeleteModal from './components/DeleteModal';
+// import { Button } from "flowbite-react";
+// import api from './api/axiosDefaults';
+// import { useEffect } from 'react';
+// import LoadingSpinner from './components/LoadingSpinner';
+// import DeleteModal from './components/DeleteModal';
 
 const App = () => {
   // Quick check that api fetching is working
-  useEffect(() => {
-    api.get('/tasks/')
-      .then(res => console.log('API response:', res.data))
-      .catch(err => console.error('Fetch error:', err));
-  }, []);
+  // useEffect(() => {
+  //   api.get('/tasks/')
+  //     .then(res => console.log('API response:', res.data))
+  //     .catch(err => console.error('Fetch error:', err));
+  // }, []);
   
   return (
     <>
-      <DeleteModal />
+      <h1 className='font-sans text-4xl font-bold tracking-wide'>Task Manager</h1>
     </>
   )
 }
