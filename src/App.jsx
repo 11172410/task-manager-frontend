1adcdada9dc67185c6f40197226f9bf573616800
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-25 12:23:47 +0400

    check api data is fetched correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import { Button } from "flowbite-react";
+import api from './api/axiosDefaults';
+import { useEffect } from 'react';
 
 const App = () => {
+  // Quick check that api fetching is working
+  useEffect(() => {
+    api.get('/tasks/')
+      .then(res => console.log('API response:', res.data))
+      .catch(err => console.error('Fetch error:', err));
+  }, []);
   
   return (
     <div>
