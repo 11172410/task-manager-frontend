b4d6f81e2c36b43bdcf9c566e4d3b7dae3a91e14
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-25 18:19:09 +0400

    create reusable success toast function

---
+++ src/functions/toasts.js
@@ -0,0 +1,15 @@
+import { toast, Bounce } from "react-toastify";
+
+export const SuccessToast = (message) => {
+  toast.success(message, {
+    position: "top-center",
+    autoClose: 3000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: false,
+    progress: undefined,
+    theme: "light",
+    transition: Bounce,
+  });
+};
