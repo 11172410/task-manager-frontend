2d545a146e35b2d6c7d970507378408c38f2499b
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-26 21:48:35 +0400

    create refresh trigger func and pass as props

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,15 @@ import TaskList from "./components/TaskList";
 import TaskDetail from "./components/TaskDetail";
 
 const App = () => {
-  // Quick check that api fetching is working
-  // useEffect(() => {
-  //   api.get('/tasks/')
-  //     .then(res => console.log('API response:', res.data))
-  //     .catch(err => console.error('Fetch error:', err));
-  // }, []);
-
   // Stores id of selected task from task list to pass to taskDetail component
   const [selectedTaskId, setSelectedTaskId] = useState(null);
   const [taskToEdit, setTaskToEdit] = useState(null);
+  const [refreshTasks, setRefreshTasks] = useState(false);
+
+  // Simple refresh function based on the previous state
+  const triggerRefresh = () => {
+    setRefreshTasks((prev) => !prev);
+  };
 
   return (
     <>
@@ -28,13 +27,19 @@ const App = () => {
           className="w-1/3"
           taskToEdit={taskToEdit}
           clearTaskToEdit={() => setTaskToEdit(null)}
+          triggerRefresh={triggerRefresh}
         />
-        <TaskList className="w-1/3" onTaskClick={setSelectedTaskId} />
+        <TaskList
+          className="w-1/3"
+          onTaskClick={setSelectedTaskId}
+          refreshTrigger={refreshTasks}
+        />
         {selectedTaskId && (
           <TaskDetail
             className="w-1/3"
             taskId={selectedTaskId}
             onEditTask={(taskData) => setTaskToEdit(taskData)}
+            triggerRefresh={triggerRefresh}
           />
         )}
       </div>
