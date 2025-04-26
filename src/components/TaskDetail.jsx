9769ab6ddaf386c45b89df4ac3025e1fa085051a
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-26 18:41:45 +0400

    add useEffect hook to load task detail

--- src/components/TaskDetail.jsx
+++ src/components/TaskDetail.jsx
@@ -1,8 +1,34 @@
 import { Button, Card } from "flowbite-react";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../api/axiosDefaults";
 
 function TaskDetail() {
+  const [taskDetail, setTaskDetail] = useState({
+    title: "",
+    description: "",
+    due_date: "",
+    due_time: "",
+    status: false,
+  });
+  const { title, description, due_date, due_time, status } = taskDetail;
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await api.get(`/tasks/1/`);
+        setTaskDetail(data);
+        // setIsLoaded(true);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    // setIsLoaded(false);
+    handleMount();
+  }, []);
+
   return (
     <Card className="max-w-sm">
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
