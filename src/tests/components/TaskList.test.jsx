5e8e75a532d98af1d7b8d9307922915028c4a442
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-27 20:34:58 +0400

    improve test setup

--- src/tests/components/TaskList.test.jsx
+++ src/tests/components/TaskList.test.jsx
@@ -1,7 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { it, expect, describe } from "vitest";
 import TaskList from "../../components/TaskList";
-import "@testing-library/jest-dom/vitest";
 import React from "react";
 
 describe("TaskList", () => {
