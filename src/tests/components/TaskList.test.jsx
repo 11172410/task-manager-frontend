218843daea5d1a8af72429ab6ad571ad571e6090
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-27 21:20:59 +0400

    test: test task list info + v iew btns rendering

--- src/tests/components/TaskList.test.jsx
+++ src/tests/components/TaskList.test.jsx
@@ -1,12 +1,50 @@
 import { render, screen } from "@testing-library/react";
 import TaskList from "../../components/TaskList";
 import React from "react";
+import { expect, vi } from "vitest";
+
+// Mock API call outside of test suite
+vi.mock("../../api/axiosDefaults", () => ({
+  default: {
+    get: vi.fn(),
+  },
+}));
+
+import api from "../../api/axiosDefaults";
 
 describe("TaskList", () => {
+  const tasks = [
+    { id: 1, title: "test 1", due_date: "2025-05-01", due_time: "00:00" },
+    { id: 2, title: "test 2", due_date: "2025-05-02", due_time: "13:00" },
+  ];
+
   it("should render the heading Task List", () => {
     render(<TaskList />);
 
     const heading = screen.getByRole("heading");
     expect(heading).toBeInTheDocument();
+  });
+
+  it("should render tasks with titles, dates and times", async () => {
+    api.get.mockResolvedValue({ data: tasks });
+    render(<TaskList onTaskClick={vi.fn()} refreshTrigger={false} />);
+
+    // Titles
+    expect(await screen.findByText("test 1")).toBeInTheDocument();
+    expect(await screen.findByText("test 2")).toBeInTheDocument();
+
+    // Dates
+    expect(await screen.findByText("1 May 2025 00:00")).toBeInTheDocument();
+    expect(await screen.findByText("2 May 2025 13:00")).toBeInTheDocument();
+  });
+
+  it("renders a View button for each task after loading", async () => {
+    api.get.mockResolvedValue({ data: tasks });
+
+    render(<TaskList onTaskClick={vi.fn()} refreshTrigger={false} />);
+
+    // 2) await the buttons to show up:
+    const viewButtons = await screen.findAllByRole("button", { name: /view/i });
+    expect(viewButtons).toHaveLength(tasks.length);
   });
 });
