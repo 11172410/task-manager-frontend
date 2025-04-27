766db96d47c92a865c0aabb39071e61128a61a20
11172410 <mariam.ayoub13@yahoo.co.uk>
2025-04-27 22:09:53 +0400

    test: testform automatically fills fields when editing task

--- src/tests/components/TaskForm.test.jsx
+++ src/tests/components/TaskForm.test.jsx
@@ -25,4 +25,35 @@ describe("TaskForm", () => {
     expect(dueDateInput).toHaveValue("");
     expect(dueTimeInput).toHaveValue("");
   });
+
+  it("should automatically fill out form fields when editing a task", () => {
+    const mockTask = {
+      id: 1,
+      title: "Edit Task",
+      description: "Edit this description",
+      due_date: "2025-05-01",
+      due_time: "14:00",
+    };
+
+    render(
+      <TaskForm
+        taskToEdit={mockTask}
+        triggerRefresh={vi.fn()}
+        clearTaskToEdit={vi.fn()}
+      />,
+    );
+
+    const titleInput = screen.getByLabelText(/title/i);
+    const descriptionInput = screen.getByLabelText(/description/i);
+    const timeInputs = screen.getAllByPlaceholderText("00");
+
+    expect(titleInput).toHaveValue(mockTask.title);
+    expect(descriptionInput).toHaveValue(mockTask.description);
+
+    expect(timeInputs.length).toBe(2);
+    expect(timeInputs[0]).toHaveValue(14);
+    expect(timeInputs[1]).toHaveValue(0);
+
+    screen.debug();
+  });
 });
